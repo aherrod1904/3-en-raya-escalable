18a9f04b05f298323dfbaa908cbcd1fb8f604b7b
Abel-dev-back <aherrod1904@g.educaand.es>
2024-02-21 18:29:50 +0100

    last finish commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   base: ``,
+  
   build: {
-    outDir: './docs'
-  }
-}
+    outDir: './docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        html1: resolve(__dirname, 'html/retrogames.html'),
+        html2: resolve(__dirname, 'html/riseofkingdoms.html'),
+        img1: resolve(__dirname, 'img/80millionpower.png'),
+        img2: resolve(__dirname, 'img/anuncio-movil.png'),
+        img3: resolve(__dirname, 'img/juegos-retro-80.jpg'),
+      },
+      
+    },
+  },
+})
+
+
